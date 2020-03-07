d7c620508a592282ad8b99a720e98a0fbec10d36
gittompotter <tom.potter@durham.ac.uk>
2020-03-07 23:23:16 +0000

    implemented passive replication

--- BEServer2.py
+++ BEServer2.py
@@ -1,15 +1,30 @@
 # python -m Pyro4.naming
 import Pyro4
+import json
+import urllib
+import urllib.request
 
 
 @Pyro4.expose
 # services program in class (name server)
 class BackEnd(object):
+    def test(self):
+        return 1
+
     def order(self, item):
         return "Ordering your {}!".format(item)
+
     def foodTypes(self):
         types = ["British", "Italian", "Indian", "Chinese", "Burgers"]
         return types
+
+    def getAddress(self, postcode):
+        postcode = postcode.replace(" ", "+")
+        url = 'http://api.getthedata.com/postcode/' + postcode
+        req = urllib.request.urlopen(url)
+        resp_str = req.read().decode('utf-8')
+        resp_js = json.loads(resp_str)
+        return resp_js
 
 
 # locate name server and link it to this RMI server
--- FEServer.py
+++ FEServer.py
@@ -5,27 +5,29 @@ hungry1 = Pyro4.Proxy("PYRONAME:primaryBE")
 hungry2 = Pyro4.Proxy("PYRONAME:secondaryBE1")
 hungry3 = Pyro4.Proxy("PYRONAME:secondaryBE2")
 
+def check():
+    try:
+        resp = hungry1.test()
+        return hungry1
+    except Exception:
+        try:
+            resp = hungry2.test()
+            return hungry2
+        except Exception:
+            try:
+                resp = hungry3.test()
+                return hungry3
+            except Exception:
+                exit(404)
 
 @Pyro4.expose
 class FrontEnd(object):
     def request(self, req):
+        hungry = check()
         print("Thanks for your request: " + req)
         resp = None
         if req == "list food types":
-            try:
-                resp = hungry1.foodTypes()
-                print("Response given from primary server")
-            except Exception:
-                try:
-                    resp = hungry2.foodTypes()
-                    print("Response given from secondary server 1")
-                except Exception:
-                    try:
-                        resp = hungry3.foodTypes()
-                        print("Response given from secondary server 2")
-                    except Exception:
-                        print("All servers down")
-                        exit(404)
+            resp = hungry.foodTypes()
         return resp
 
 
